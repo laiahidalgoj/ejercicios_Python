61504728d73537885e7868092e110804935867a7
Laia <laia@hidalgojimenez.es>
2022-03-13 10:00:06 +0100

    added exercise 1 'intro datos'

---
+++ 3. Intro datots python/variables.py
@@ -0,0 +1,19 @@
+nombre = "Laia"
+apellidos = "Hidalgo Jiménez"
+edad = "30"
+email = "laia@laia.es"
+telefono = "699999999"
+casado = True
+hijos = False
+listaAmigos = ["Estefania", "Clara", "Montse", "Judit", "Ernesto", "Fran"]
+peliculasVistas = {1: "Resident Evil", 2: "Uncharted"}
+
+print(nombre)
+print(apellidos)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(hijos)
+print(listaAmigos)
+print(peliculasVistas)
